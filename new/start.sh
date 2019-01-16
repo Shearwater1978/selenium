64b6d609db8427a4064854b5aed58d5de1249b20
Uglykoyote <uglykoyote@yandex.ru>
2019-01-16 16:24:09 +0400

    Create start.sh

---
+++ new/start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -x
+
+export PATH="/home/toptop/.pyenv/bin/:$PATH"
+export PATH="/usr/local/bin/geckodriver:$PATH"
+
+eval "$(pyenv init -)"
+
+pyenv local 3.6.0
+pyenv shell 3.6.0
+
+python selenium_try.py
