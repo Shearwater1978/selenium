b72372f818f0316f4f1941f4ea4caaf4eb7614fb
Uglykoyote <uglykoyote@yandex.ru>
2019-01-18 16:35:07 +0400

    Update clean.sh

--- new/clean.sh
+++ new/clean.sh
@@ -2,7 +2,7 @@
 
 file_name="./dodo_arch/dodo_"$(date +%d-%m-%y_%H%M)".zip"
 echo $file_name
-find ./dodo_body/ -mmin +30 -name '*.html' -exec zip -q -9 ./dodo_arch/dodo_"$(date +%d-%m-%y_%H%M)".zip {} \;
+find ./dodo_body/ -mmin +60 -name '*.html' -exec zip -q -9 ./dodo_arch/dodo_"$(date +%d-%m-%y_%H%M)".zip {} \;
 
 archive_state=$(unzip -q -t "$file_name" | awk '{print $1,$2,$3}')
 echo $archive_state
@@ -10,7 +10,7 @@ if [ "$archive_state" = "No errors detected" ]
    then
        echo 'Zip archive ok'
        echo 'Source file(-s) must be remove'
-       find ./dodo_body/ -mmin +30 -name '*.html' -delete
+       find ./dodo_body/ -mmin +60 -name '*.html' -delete
    else
        echo 'Zip archive not`ok'
        echo 'Source file(-s) not removed'
