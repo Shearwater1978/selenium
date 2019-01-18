1fcea249863cb6bf2b11a9eae2decf46c84e6807
Uglykoyote <uglykoyote@yandex.ru>
2019-01-18 15:15:36 +0400

    Update selenium_try.py

--- new/selenium_try.py
+++ new/selenium_try.py
@@ -10,37 +10,29 @@ import requests
 default_width = 1900
 default_height = 1080
 
-
 print(">> Start in... "+datetime.datetime.now().strftime("%d-%m-%Y %H:%M"))
 
-
-print("Start vDisplay")
 # Set screen resolution to 1366 x 768 like most 15" laptops
 display = Display(visible=0, size=(default_width, default_height))
 display.start()
 print("vDisplay started")
 
 # now Firefox will run in a virtual display.
-print("Get size")
 browser = webdriver.Firefox(executable_path="/usr/local/bin/geckodriver")
 browser.set_window_size(default_width, default_height)
 browser.get("https://dodocontrol.ru/checks")
 
 inputElement = browser.find_element_by_xpath('//*[@id="socialNetworkLink"]')
-inputElement.send_keys('https://vk.com/id12867863')
-
+inputElement.send_keys('https://vk.com/id')
 
 my_choice=browser.find_element_by_xpath('//*[@id="anketa"]/div[1]/div[2]/div[2]/div/button')
 my_choice.click()
 
 # set timeouts
-#browser.set_script_timeout(30)
-#browser.set_page_load_timeout(30) # seconds
-print("Waiting 30 seconds")
+print("Waiting 20 seconds")
 time.sleep(20)
 total_height = browser.execute_script("return document.body.parentNode.scrollHeight")
 browser.quit()
-
 
 # now Firefox will run in a virtual display.
 print("Open window with new size")
@@ -56,28 +48,18 @@ my_choice=browser.find_element_by_xpath('//*[@id="anketa"]/div[1]/div[2]/div[2]/
 my_choice.click()
 
 html = browser.page_source
-filename = "/home/toptop/git/selenium/dodo_body/dodo_"+datetime.datetime.now().strftime("%d-%m-%Y_%H:%M")+".html"
-
+filename = "/home/toptop/git/selenium/dodo_body/dodo_"+datetime.datetime.now().strftime("%d-%m-%Y_%H%M")+".html"
 
 with open(filename, 'w') as out_file:
     out_file.write(html)
 soup = BS(html, 'html.parser')
-#for cell in soup.select('#delivery_calendar > table > tbody > tr:nth-child(1) > td:nth-child(1)'):
 for cell in soup.find("div",{"id":"delivery_calendar"}).findAll("td"):
     if cell.get("class")[0] not in ("blocked-day","inactive-day"):
         url = 'https://sms.ru/sms/send'
-        data = ({'api_id':'id', 'to':'tel_num', 'msg':'Available day in operations!!!!'})
+        data = ({'api_id':'id', 'to':'phone_number', 'msg':'Available day in operations!!!!'})
         r = requests.post(url, data=data)
 
-# Take screenshot
-#print("Take screenshot")
-#browser.save_screenshot("dodo.png")
-
-# quit browser
 browser.quit()
-
-# quit Xvfb display
 display.stop()
-
 
 print(">> End in... "+datetime.datetime.now().strftime("%d-%m-%Y %H:%M"))
